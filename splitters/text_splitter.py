955b757634ab9cbb8398d29a04df06b819b02dd5
Rahul Tiwari <dsrahul0822@gmail.com>
2026-02-06 16:52:51 +0530

    Add files via upload

---
+++ splitters/text_splitter.py
@@ -0,0 +1,10 @@
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+def split_document(documents):
+    """
+    Split document into chunks for embedding
+    """
+    splitter = RecursiveCharacterTextSplitter(chunk_size=500,
+                                              chunk_overlap=50)
+    split_docs = splitter.split_documents(documents)
+    return split_docs
